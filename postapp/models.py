c7008e2ba818fc941282030548a860d312c88d1e
abdullah <anwerabdullah794@gmail.com>
2026-03-07 02:47:59 +0200

    creat author

--- postapp/models.py
+++ postapp/models.py
@@ -5,14 +5,19 @@ from django.utils import timezone
 
 from taggit.managers import TaggableManager
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 class Post(models.Model):
+    author=models.ForeignKey(User,related_name='post_authoe',on_delete=models.CASCADE)
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=1000)
     draft=models.BooleanField(default=True)
     publish_date=models.DateTimeField(default=timezone.now)
     tags = TaggableManager()
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
 
 
     def __str__(self):
--- project/settings.py
+++ project/settings.py
@@ -123,3 +123,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+MEDIA_URL= "media/"
+MEDIA_ROOT=  BASE_DIR / "media"
